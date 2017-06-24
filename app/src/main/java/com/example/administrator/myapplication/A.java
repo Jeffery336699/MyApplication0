137a5eb9ce2b3b90b691d324c575b75eadbcdcaf
tj <912833721@qq.com>
2017-06-24 09:56:28 +0800

    第一次

---
+++ app/src/main/java/com/example/administrator/myapplication/A.java
@@ -0,0 +1,9 @@
+package com.example.administrator.myapplication;
+
+/**
+ * Created by $tangj$ on 2017/6/24 0024 0:19.
+ */
+
+public class A {
+
+}
---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_hello_world)
+    TextView       mTvHelloWorld;
+    @BindView(R.id.activity_main)
+    RelativeLayout mActivityMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tv_hello_world)
+    public void onViewClicked() {
+
+    }
+}
