f16ecad7fb311f0690bc48f418ebeb0052419c1e
frank <chenguozhuhh@163.com>
2017-06-24 10:36:39 +0800

    hello world

---
+++ app/src/main/java/com/example/administrator/myapplication/TestClass.java
@@ -0,0 +1,18 @@
+package com.example.administrator.myapplication;
+
+/**
+ * 创建者     frank
+ * 创建时间   2017/6/24 10:34
+ * 描述	      ${TODO}
+ * 更新者     $Author$
+ * 更新时间   $Date$
+ * 更新描述   ${TODO}
+ */
+
+public class TestClass {
+
+
+    public static void test(){
+        System.out.println("junjunshi ");
+    }
+}
