99d5eeb3381f4582c17e67a09285a0ff69ab76ed
tangj <912833721@qq.com>
2017-09-04 17:26:15 +0800

    发布到远程dev

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -11,6 +12,7 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     @BindView(R.id.tv_hello_world)
     TextView       mTvHelloWorld;
     @BindView(R.id.activity_main)
@@ -25,6 +27,6 @@ public class MainActivity extends AppCompatActivity {
 
     @OnClick(R.id.tv_hello_world)
     public void onViewClicked() {
-
+        Log.i(TAG, "onViewClicked: ----推送到远程dev分支");
     }
 }
